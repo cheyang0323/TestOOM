58c482cc50623d44b62f7658569c86cbefd83f8d
David Yang <david.yang@oracle.com>
2020-11-04 15:53:29 +0800

    updated

--- src/main/java/com/oom/TestOOM/Controller/TestController.java
+++ src/main/java/com/oom/TestOOM/Controller/TestController.java
@@ -10,27 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @GetMapping("/{name}")
     public String tryIt(@PathVariable String name) throws Exception {
-        Long maxMemory = Runtime.getRuntime().maxMemory();
-        System.out.println("Total memory: " + maxMemory);
-        int[] matrix = new int[(int) (maxMemory + 1024000)];
-        for (int i = 0; i < matrix.length; ++i) {
-            matrix[i] = i + 1;
-        }
-        int iteratorValue = 20;
-        System.out.println("\n=================> OOM test started..\n");
-        for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
-            System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
-            int loop1 = 2;
-            int[] memoryFillIntVar = new int[iteratorValue];
-            // feel memoryFillIntVar array in loop..
-            do {
-                memoryFillIntVar[loop1] = 0;
-                loop1--;
-            } while (loop1 > 0);
-            iteratorValue = iteratorValue * 5;
-            System.out.println("\nRequired Memory for next loop: " + iteratorValue);
-            Thread.sleep(100);
-        }
+        long[][] ary = new long[Integer.MAX_VALUE][Integer.MAX_VALUE];
         return "Hello, " + name;
     }
 
