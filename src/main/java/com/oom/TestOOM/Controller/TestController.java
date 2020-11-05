cd12a82bdd4ff24d930e8f1619a478804b58e18d
David Yang <david.yang@redhat.com>
2020-11-05 21:13:45 +0800

    add jpa

--- src/main/java/com/oom/TestOOM/Controller/TestController.java
+++ src/main/java/com/oom/TestOOM/Controller/TestController.java
@@ -1,19 +1,42 @@
 package com.oom.TestOOM.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.oom.TestOOM.Repository.ReservationRepository;
+import com.oom.TestOOM.Repository.Reservation;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 @RequestMapping("/api/v1/oom")
 public class TestController {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
     @GetMapping("/{name}")
     public String tryIt(@PathVariable String name) throws Exception {
 
         // update
         long[][] ary = new long[Integer.MAX_VALUE][Integer.MAX_VALUE];
         return "Hello, " + name + "\n";
+    }
+
+    @PostMapping("/reserve")
+    public @ResponseBody String reserve(@RequestParam String name, @RequestParam String date,
+            @RequestParam Boolean hasCard) {
+        Reservation reservation = new Reservation();
+        reservation.setName(name);
+        reservation.setDate(date);
+        reservation.setHasCard(hasCard);
+        reservationRepository.save(reservation);
+        return "done";
     }
 
     @GetMapping("/good")
---
+++ src/main/java/com/oom/TestOOM/Repository/Reservation.java
@@ -0,0 +1,50 @@
+package com.oom.TestOOM.Repository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String date;
+    private Boolean hasCard;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(final String date) {
+        this.date = date;
+    }
+
+    public Boolean getHasCard() {
+        return hasCard;
+    }
+
+    public void setHasCard(final Boolean hasCard) {
+        this.hasCard = hasCard;
+    }
+}
